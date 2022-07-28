9bc5d72249c0f1dbcb2c04ff4e74e09fd392ec23
Muhammad Aahad <maahad@asaadshub.com>
2022-07-28 19:40:39 +0500

    Added mongodb

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 const http = require("http");
+const db = require("./db/config");
 
-
-
+require("dotenv").config();
 
 var app = express();
 
@@ -13,15 +13,18 @@ app.use("/ping", function (req, res, next) {
   res.status(200).send("pong");
 });
 
-app.set("port", process.env.APP_PORT || 5012);
+app.set("port", 5012);
 
 var server = http.createServer(app);
 
+db.connect().then(() => {
+  server.listen(8080, function () {
+    console.log(
+      "Example app listening on port 8080!! CI/CD FOR STAGING COMPLETE!!! Go to https://localhost/"
+    );
+  })
 
-server.listen(8080, function () {
-  console.log(
-    "Example app listening on port 8080!! CI/CD FOR STAGING COMPLETE!!! Go to https://localhost/"
-  );
 })
+
 
 module.exports = app;
---
+++ db/config.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const logger = require("tracer").console();
+
+const connect = () => {
+  return new Promise((resolve) => {
+    try {
+      mongoose.connect(
+        process.env.MONGO_URL,
+        (err) => {
+          if (err) {
+            logger.log(`Error connecting to Database: ${err}`);
+            process.exit(1);
+          } else {
+            logger.log("Connected to Database...");
+            resolve();
+          }
+        }
+      );
+    } catch (err) {
+      logger.log(`Error connecting to Database: ${err}`);
+      process.exit(1);
+    }
+  });
+};
+
+const close = () => {
+  return mongoose.disconnect()
+}
+
+module.exports = {connect,close};
